916965fe5bda4c5add2ba876aaa2b872bed3320f
Illya Petrov <ilya.muromec@gmail.com>
2020-04-17 23:28:22 +0200

    Updates

---
+++ precache-manifest.a23a2a48cef70e18b954b347f6513d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f35d7a312aaf689c752fb3c67938395",
+    "url": "/signerbox2/index.html"
+  },
+  {
+    "revision": "0df4dbbfab044814cfe6",
+    "url": "/signerbox2/static/css/main.ec83df7c.chunk.css"
+  },
+  {
+    "revision": "18ba7eb342734581162d",
+    "url": "/signerbox2/static/js/2.a87cc285.chunk.js"
+  },
+  {
+    "revision": "3cda86e46cf63f3a489046f2266ea3af",
+    "url": "/signerbox2/static/js/2.a87cc285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df4dbbfab044814cfe6",
+    "url": "/signerbox2/static/js/main.71fcb77c.chunk.js"
+  },
+  {
+    "revision": "28a49ddb510daf60147e",
+    "url": "/signerbox2/static/js/runtime-main.95dd0ea8.js"
+  }
+]);
